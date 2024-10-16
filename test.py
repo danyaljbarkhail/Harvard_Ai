88fb3c9fd23e7d0e807abbed9f73c23c31ccac84
Danyal Jabarkhail <149615447+danyaljbarkhail@users.noreply.github.com>
2024-10-16 21:13:55 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,6 @@
+from traffic import load_data
+
+# Use smaller modified dataset for testing
+data_directory = "/Users/ronaldokeng/Desktop/2021 UNSW/Extracurricular/CS50x AI/pset5/traffic/gtsrb-small"
+
+load_data(data_dir=data_directory)
